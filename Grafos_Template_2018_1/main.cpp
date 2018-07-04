2e659914f2e6f8a5502dea1dcb73d332aab94a34
Vini <viniciusalves@ice.ufjf.br>
2018-07-04 17:09:56 -0300

    gerador de instancias no mais e varias instancias novas

--- Grafos_Template_2018_1/main.cpp
+++ Grafos_Template_2018_1/main.cpp
@@ -4,6 +4,28 @@
 
 int main(int argc, char *argv[])
 {
+    string path = "instancias/instancia.txt" ;
+    ofstream f(path);
+    int n = 32;
+    if(f.is_open()){
+        f << n;
+        f << "\n";
+        for(int i=1;i<=n;i++){
+            for ( int j=1;j<=n;j++){
+                if(j!=i && i<j){
+                    f << i;
+                    f << " ";
+                    f << j;
+                    f << " ";
+                    f << 0;
+                    f << "\n";
+                }
+            }
+
+    }
+    } else {
+        cerr << "Couldn't open file!" << endl;
+    }
     Grafo grafo = Grafo();
     grafo.readFile("instancias/K5.txt");
     //grafo.showInfo();
